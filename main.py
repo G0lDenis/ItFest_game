df0d4c33f1f4dcfe06027e96d4368eeb5efbeb7b
Fesmat <dden4929@gmail.com>
2020-12-04 20:34:55 +0300

    Classes added. Without blit.

--- main.py
+++ main.py
@@ -8,64 +8,89 @@ def init_main_screen(screen):
 
 
 def show_start_menu(screen):
-    global st_but
-    global set_but
-    image = pygame.image.load('Images/menu background 2.png').convert()
-    image = pygame.transform.scale(image, screen.get_size())
-    rect = image.get_rect()
-    rect.center = (image.get_size()[0] // 2,
-                   image.get_size()[1] // 2)
-    screen.blit(image, rect)
-    image = pygame.image.load('Images/start button 3.png').convert()
-    image.set_colorkey((255, 255, 255))
-    rect = image.get_rect()
-    rect.center = (0.3 * screen.get_size()[0] + image.get_size()[0] // 2,
-                   0.4 * screen.get_size()[1] + image.get_size()[1] // 2)
-    st_but = screen.blit(image, rect)
-    image = pygame.image.load('Images/setup button 4.png').convert()
-    image.set_colorkey((255, 255, 255))
-    rect = image.get_rect()
-    rect.center = (0.6 * screen.get_size()[0] + image.get_size()[0] // 2,
-                   0.4 * screen.get_size()[1] + image.get_size()[1] // 2)
-    set_but = screen.blit(image, rect)
+    global button_start
+    global button_setup
+    global sprites
+    image_bg_menu = pygame.image.load('Images/menu background 2.png').convert()
+    image_bg_menu = pygame.transform.scale(image_bg_menu, screen.get_size())
+    rect = image_bg_menu.get_rect()
+    rect.center = (image_bg_menu.get_size()[0] // 2,
+                   image_bg_menu.get_size()[1] // 2)
+    screen.blit(image_bg_menu, rect)
+    button_start = MenuButton('start')
+    sprites.add(button_start)
+    sprites.draw(screen)
+    button_setup = MenuButton('setup')
+    sprites.add(button_setup)
+    screen.blit(button_start, button_start.rect_center())
+    screen.blit(button_setup, button_setup.rect_center())
+    sprites.draw(screen)
 
 
 def process_event(screen):
     global running
     global present_screen
-    global st_but
-    global set_but
+    global button_start
+    global button_setup
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if present_screen == 'Menu':
-                if st_but.collidepoint(event.pos):
-                    game_start(screen)
-                if set_but.collidepoint(event.pos):
-                    open_settings(screen)
+                if button_start.collidepoint(event.pos):
+                    game_start()
+                if button_setup.collidepoint(event.pos):
+                    open_settings()
             if present_screen == 'Game':
                 pass
 
 
-def game_start(screen):
+def game_start():
+    global screen
     global present_screen
+    global button_start
+    global button_setup
+    button_start.kill()
+    button_setup.kill()
     present_screen = 'Game'
 
 
-def open_settings(screen):
+def open_settings():
+    global screen
     global present_screen
     present_screen = 'Settings'
 
 
+class MenuButton(pygame.sprite.Sprite):
+    def __init__(self, bt_name):
+        self.bt_name = bt_name
+        super().__init__()
+        if bt_name == 'start':
+            self.image = pygame.image.load('Images/start button 3.png').convert()
+        else:
+            self.image = pygame.image.load('Images/setup button 4.png').convert()
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
+
+    def rect_center(self):
+        global screen
+        if self.bt_name == 'start':
+            return (0.3 * screen.get_size()[0] + self.image.get_size()[0] // 2,
+                    0.4 * screen.get_size()[1] + self.image.get_size()[1] // 2)
+        else:
+            return (0.6 * screen.get_size()[0] + self.image.get_size()[0] // 2,
+                    0.4 * screen.get_size()[1] + self.image.get_size()[1] // 2)
+
+
 if __name__ == '__main__':
     pygame.init()
+    running = True
+    present_screen = 'Menu'
+    button_start = None
+    button_setup = None
+    sprites = pygame.sprite.Group()
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     init_main_screen(screen)
-    running = False
-    present_screen = 'Menu'
-    st_but = None
-    set_but = None
     while running:
         process_event(screen)
         pygame.display.flip()
